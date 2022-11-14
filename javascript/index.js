1430370c5f691e4a4025b19bfbf67d4b796d8f3c
gyamficodes <you@example.com>
2022-11-14 07:05:07 +0000

    lesson update

--- javascript/index.js
+++ javascript/index.js
@@ -218,9 +218,11 @@ console.log(typeof p)
  const bu = [1,2,3,4,5,6,7,8,9,10]
  const letters = ['a','d','c']
 
-
 const one =[farms, bu, letters];
+// one [][] = 'tom';
 console.log(one);
+
+
 
 
 // && and || it helps us to check more than one conditions
@@ -277,15 +279,175 @@ function checkwiner(win){
 
 
 
-//varriables scope == where we can access varriables
+// //varriables scope == where we can access varriables
 
-for(let v = 0; v < 3;v+=1){
-    console.log(v)
-}//let varriables the console always dey inside
+// for(let v = 0; v < 3; v+=1){
+//     console.log(v)
+// }//let varriables the console always dey inside
 
-for(let c = 0; c < 3;v+=1){
-    console.log(c)
-}//let varriables the console always dey outside
+// for(var m = 0; m < 3; v+=1){
+// }//let varriables the console always dey outside
+// console.log(m)
+
+
+
+// template leterals === delimited with (`)
+let user = 'john';
+console.log(`hello ${user}`)
+
+
+// tolocalstring(local, {option})
+//locale it specify the language
+//option it object is formating string
+
+let mynum = 1234.5678;
+
+// mynum = mynum.toLocaleString('en-US'); usa
+// mynum = mynum.toLocaleString('hi-IN'); //hindi
+// mynum = mynum.toLocaleString('de-DE'); //standard german
+
+
+
+ mynum = mynum.toLocaleString(undefined, {style: 'currency', currency: "USD"}); 
+// mynum = mynum.toLocaleString('hi-IN'); //hindi
+console.log(mynum);
+
+
+//Arrays ==they can store multiples values
+
+let foodstaff = ['apple','banana','mango']
+let foodstaf = ['apple','banana','mango']
+// foodstaff[0] = 'beans';.
+//foodstaff.push('fish') adding an element to the last of array.
+// foodstaff.pop() remove last element of an array.
+// foodstaff.unshift('pawpaw') adding an element to the begining of array.
+//foodstaff.shift() remove last element of an array.
+//console.log(foodstaff.length) checking tht amount of element in the array.
+//console.log(foodstaff.indexOf('mango')) checking the position of that Element.
+//console.log(foodstaff.concat(foodstaf)) adding two different arrays together.
+//foodstaff.splice(2, 0, "Lemon", "Kiwi"); method can be used to add new items to an array:
+//foodstaff = foodstaff.sort(); arrangement of the Element. eg alphabatical order
+//foodstaff.sort().reverse() it reverse the alphabatical order to it previous state.
+
+console.log(foodstaff);
+console.log(foodstaf);
+
+
+
+//spread operators ... it used to expand arrays n strings.
+ const pass =[1,2,3,4,5,6,7]
+console.log(...pass)
+
+const helo = 'thomas';
+console.log(...helo);
+
+
+//rest operator === represents an indefinet number of parameters
+//(packs aguments into arrays)
+
+let b = 2;
+let k = 2;
+let o = 2;
+let x = 2;
+
+console.log(sum(b, k, o, x));
+
+function sum(b, k, ...numbers){
+    let total = 0;
+    for(number of numbers){
+        total += number
+    }
+    return total
+}
+
+
+
+// callbackfunctions == a function passed as an aguments
+// it ensures that a functions is not going to work until task is completed
+//eg wait for the file to load
+
+
+
+
+
+
+
+
+// for each
+
+
+
+
+// array.map == it loops and returns array;
+
+let students = ['nana yaw', 'monica','john']
+
+
+
+ //array.filter ===
+
+ const numers = [1,2,3,4,5,6,7]
+
+console.log(numers.filter(numers => numers> 2))
+
+
+
+
+// reduce() === reducess an array to a single value [sum]
+
+
+
+//function expreaion == funtion without a name (anonymous function)
+
+// sayhello();
+
+// function sayhello(){
+//     console.log('hello')
+// }
+
+let counting = 0;
+
+
+function decreasecounting(){
+    counting -=1;
+    document.getElementById('label').innerHTML = counting;
+}
+
+function increasecounting(){
+    counting +=1;
+    document.getElementById('label').innerHTML = counting;
+}
+
+
+let usernamee;
+
+function personusernamee(){
+ usernamee =   document.getElementById('myinputt').value;
+    console.log(usernamee)
+}
+
+
+let checkes;
+
+function mycheckcheckes(){
+    if(document.getElementById('mychecboxx').checked == true){
+        alert('you have subscribe')
+    }else{
+        alert('you have not subscribe')
+    }
+}
+
+
+// Arrow functions === its a compact to a traditonal alternative func.
+
+
+
+
+
+
+
+
+
 
 
 
