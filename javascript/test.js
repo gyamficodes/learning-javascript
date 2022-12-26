f3e08bbcfe1a4f0111fc45697f6cd7f4358ff8ab
gyamficodes <gyamfijohn08@gmail.com>
2022-12-26 10:43:31 -0800

    main

--- javascript/test.js
+++ javascript/test.js
@@ -96,7 +96,7 @@
 //     console.log(p)
 // }
 
-let pie = ['q','k',12]
+let pie = ['q','k', 'gg','bb']
 
 // for(e of pie){
 //     console.log(e)
@@ -140,6 +140,146 @@ let pie = ['q','k',12]
 // console.log(pie.map((a, b) => {
 //     return a +  b
 // }))
+
+
+//function
+
+let age = 20;
+let love = 90;
+
+function greeting(a, b){
+    let result = a + b;
+    console.log( result ) 
+}
+
+greeting(age , love)
+
+
+//nested fucn
+
+let log = 'gyamfi john'
+let sms = 10;
+
+
+function  smspage(){
+   displaylogin();
+   displaysms();
+
+   function displaylogin(){
+    console.log(`you wellcome${log}`)
+}
+   function displaysms(){
+    console.log(`you have recieve${sms}messages`)
+   }
+}
+
+smspage();
+
+
+
+//arrow func
+const shake = () => 'pls if ur greeting shake my hands';
+let answer = shake();
+console.log(answer)
+
+
+//settimeout
+let time = new Date()
+
+function morning() {
+    console.log(`come to my house on ${time}sharp`)
+}
+
+
+
+setTimeout(morning(), 3000)
+
+
+
+//class n contructor
+class person{
+    constructor(name,gender,country,contact){
+        this.name = name;
+        this.gender = gender;
+        this.country = country;
+        this.contact = contact;
+    }
+}
+
+const person1 = new person('john','male','country','0558733370');
+console.log(person1);
+
+
+
+const code = 'jonny@2022';
+
+if(code >= 8 || code.includes('@') || code >= 9){
+    console.log('you have a strong password!')
+}else if(code < 8 && code.includes(!'@')  ) {
+    console.log('you have problem in your passworg')
+}else if(code <= 7){
+    console.log('bad password try again')
+}else{
+    console.log('you have no password')
+}
+
+//switch case
+let student =  100;
+
+switch(student){
+case student = 100:
+    console.log('exellent points!')
+break;
+case student = 80:
+    console.log('very good points!')
+break;
+case student = 60:
+    console.log('good points!')
+break;
+case student = 50:
+    console.log('pass mark points!')
+break;
+case student = 40:
+    console.log('credit points!')
+break;
+case student = 20:
+    console.log('you fail!')
+break;
+default:{
+    console.log('you did not take part in  the exams')
+}
+}
+
+
+// weather app
+
+
+let  weather = window.prompt('how is the weather today?')
+
+if(weather == 'rain'){
+    console.log('grab your umbrella!')
+}else if(weather == 'sunny'){
+    console.log('you do not need umbrella!')
+}else{
+    console.log('goodnight!')
+}
+
+
+
+//dom
+
+let st = 'rbgksfhgdbhfgbfjbfjgbfu'
+
+let mytext = document.getElementById('vb');
+console.log(mytext)
+mytext.setAttribute('style' , 'color:red;')
+ 
+let paper = document.getElementById('vb');
+let pp = '';
+pp+=`<span>${st}`
+paper.innerHTML = pp;
+
+paper.setAttributeNS()
 
 
 
