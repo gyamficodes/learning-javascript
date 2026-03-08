2081376be50cba925d91bb62467c85235a26fd81
gyamficodes <gyamfijohn08@gmail.com>
2026-03-08 23:52:36 +0000

    Add to-do list application with add and delete task functionality

---
+++ Projects/EventListner/todo.js
@@ -0,0 +1,36 @@
+let tasks = [];
+
+//function nto display task
+
+const displayTask = () => {
+  let html = "";
+  for (let i = 0; i < tasks.length; i++) {
+    html += `<li>${tasks[i]} <button  onclick="removeTask(${i})">Delete</button></li>`;
+    // document.getElementById("list").innerHTML = html;
+  }
+  document.getElementById("list").innerHTML = html;
+};
+
+
+
+//function to add task
+const addTask = () => {
+    let taskInput = document.getElementById("task");
+    let text = taskInput.value;
+    if(text !== ""){
+       tasks.push(text);
+        displayTask();
+        text.value = "";
+        // saveTask();
+        displayTask();
+    }
+    
+ 
+}
+
+///function to delete task
+const removeTask = (i) => {
+    tasks.splice(i, 1)
+    //   saveTask();
+    displayTask();
+}
