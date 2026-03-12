2ba3391175f0e45be185f2cc38f5525551fb707c
gyamficodes <gyamfijohn08@gmail.com>
2026-03-12 00:03:00 +0000

    build modal-overlay popup feature with event listeners

--- Projects/Modal/modal.js
+++ Projects/Modal/modal.js
@@ -1,14 +1,37 @@
 
 let  modal = document.getElementById("modal");
-let closeBtn = document.getElementsByClassName("closeBtn");
+let closeBtn = document.getElementById("closeBtn");
 let openBtn  = document.getElementById("openBtn");
 
 
-
-
+// function to open modal
 const handleOpenModal = () => {
     modal.classList.add("show");
 }
 
 
+// function to close modal
+const handleCloseModal = () => {
+    modal.classList.remove("show");
+}
+
+
+// event listener
 openBtn.addEventListener("click", handleOpenModal);
+closeBtn.addEventListener("click", handleCloseModal);
+
+
+// close modal when click outside of modal
+modal.addEventListener('click', (event) => {
+    if(event.target === modal){
+        handleCloseModal();
+    }
+})
+
+
+// close modal when press escape key
+document.addEventListener("keydown", (event) => {
+    if(event.key === "Escape"){
+        handleCloseModal();
+    }
+})
