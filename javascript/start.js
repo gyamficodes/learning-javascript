ffd3eacde11a3efe9be32487c99036f7fe6b9d3a
gyamficodes <gyamfijohn08@gmail.com>
2026-05-14 21:47:59 +0100

    update

--- javascript/start.js
+++ javascript/start.js
@@ -1427,8 +1427,6 @@ function displayScreenInfo(){
 displayScreenInfo();
 
 
-
-
 // JavaScript Window Location
 // The window.location object can be used to get the current page address (URL) and to redirect the browser to a new page.
 
